b2543bef8ec8e1ede667042020ea2884fc990bb8
thiruraje <vklthiru2000@gmail.com>
2020-06-24 08:42:10 -0700

    first commit

---
+++ config/services.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+    'google' => [
+        'client_id' => '141775970080-pf73hrjj5qthihhvjkcj80099vduornh.apps.googleusercontent.com',
+        'client_secret' => 'OHBZI0VGpZ1V817De8Eclopg',
+        'redirect' => 'http://localhost:8000/callback'],
+
+];
